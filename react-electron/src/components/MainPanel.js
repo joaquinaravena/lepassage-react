0a69e7e1e55a29523544a8b573f9566a1f9783be
Juan Ignacio <juanirmariani@gmail.com>
2024-07-29 13:27:32 -0300

    Add scroll for tabs and tables and create bottons for options to edit, add and delete elements from the table.

--- react-electron/src/components/MainPanel.js
+++ react-electron/src/components/MainPanel.js
@@ -8,48 +8,51 @@ import PaquetesTable from "./tables/PaquetesTable";
 import EtiquetasTable from "./tables/EtiquetasTable";
 import ProductosTable from "./tables/ProductosTable";
 import SearchBar from "./SearchBar";
+import "./styles/styleBar.css"
 
-export default function MainPanel(className) {
+export default function MainPanel({ className }) {
     return (
-        <div className={"w-full text-black h-screen bg-[hsl(100,100,100)] overflow-clip " + className}>
-            <Tabs className="h-full overflow-clip flex flex-col"
-                  selectedTabPanelClassName="react-tabs__tab-panel--selected grow overflow-clip"
+        <div className={"w-full text-black h-screen bg-[hsl(100,100,100)] overflow-hidden " + className}>
+            <Tabs className="h-full flex flex-col"
+                  selectedTabPanelClassName="react-tabs__tab-panel--selected grow overflow-auto"
                   selectedTabClassName="bg-gray-500 rounded-t-xl rounded-tr-xl">
-                <div className="flex items-center justify-center relative">
-                    <TabList className="flex justify-center mb-4 space-x-4">
-                        <Tab>Insumos</Tab>
-                        <Tab>Liquidos</Tab>
-                        <Tab>Envases</Tab>
-                        <Tab>Miscelaneos</Tab>
-                        <Tab>Paquetes</Tab>
-                        <Tab>Etiquetas</Tab>
-                        <Tab>Productos</Tab>
-                        <div className="absolute right-11 mt-2">
-                            <SearchBar/>
-                        </div>
-                    </TabList>
+                <div className="flex items-center justify-between relative">
+                    <div className="flex-grow overflow-x-auto">
+                        <TabList className="flex justify-center sm:justify-start mb-4 space-x-4 whitespace-nowrap">
+                            <Tab>Insumos</Tab>
+                            <Tab>Liquidos</Tab>
+                            <Tab>Envases</Tab>
+                            <Tab>Miscelaneos</Tab>
+                            <Tab>Paquetes</Tab>
+                            <Tab>Etiquetas</Tab>
+                            <Tab>Productos</Tab>
+                        </TabList>
+                    </div>
+                    <div className="flex-shrink-0 ml-4">
+                        <SearchBar />
+                    </div>
                 </div>
 
-                <TabPanel className="overflow-clip">
-                    <InsumosTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <InsumosTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <LiquidosTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <LiquidosTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <EnvasesTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <EnvasesTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <MiscelaneosTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <MiscelaneosTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <PaquetesTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <PaquetesTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <EtiquetasTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <EtiquetasTable className="h-full" />
                 </TabPanel>
-                <TabPanel className="overflow-clip">
-                    <ProductosTable className="h-full"/>
+                <TabPanel className="overflow-y-auto">
+                    <ProductosTable className="h-full" />
                 </TabPanel>
             </Tabs>
         </div>
